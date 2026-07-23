c43ab18e595e476bcb6d7f13c81fee3e075995b6
Harshit-638 <harshitsharma3264@gmail.com>
2026-07-23 11:00:57 +0530

    Day 1 JS

---
+++ Day1/day1.js
@@ -0,0 +1,37 @@
+// function in JS : block of code
+// syntax :
+// function fname(){
+
+//}
+//fname();
+
+// function add(num1,num2){
+// console.log(num1 +num2);
+// return num1+num2;
+// }
+
+// add(2,1);
+// function add(num1,num2){
+
+// return num1+num2;
+// }
+// add(2,1);
+
+// // arrow function 
+// // syntax : ()=>{}
+
+//     const add=()=> {
+//         console.log("arrow function")
+//     }
+//     add();
+//     const add=()=> {
+//         return num1 + num2;
+//     }
+//     console.log(add(2,1));
+
+function addNum(){
+
+    console.log(arguments);
+}
+ addNum(1,2,3,4,5,6,8);
+ 
