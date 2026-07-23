d38434ade1b83648f0cb9480c183e739144c79e2
Harshit-638 <harshitsharma3264@gmail.com>
2026-07-23 11:12:08 +0530

    Day 2 JS

--- Day2/day2.js
+++ Day2/day2.js
@@ -1,4 +1,4 @@
-//synchronous programming : code is executed line  by line
+// synchronous programming : code is executed line  by line
 // function hello(){
 //     console.log("hello,world");
 
@@ -6,8 +6,8 @@
 // hello();
 // console.log("This is synchronous programming");
 
-//Asynchornous programming : the code is executed line by line, but some 
-//setTimeout(): function that is used to execute the function 
+// Asynchornous programming : the code is executed line by line, but some 
+// setTimeout(): function that is used to execute the function 
 
 // const hello =()=> {
 //     setTimeout(() => {
@@ -19,23 +19,31 @@
 
 // callback, promises, async/await
 
-function add(n1,n2,callback){
-    console.log (n1+n2);   
-    callback();
-}
+// function add(n1,n2,callback){
+//     console.log (n1+n2);   
+//     callback();
+// }
 
-let a =10;
-let b = 20;
-function sayHI(){
-    console.log("This is callback funtion");
-}
+// let a =10;
+// let b = 20;
 
-add(a,b,sayHI);
-add(a,b,hello);
-add(hello,sayHI);
+// add(a,b,sayHI);
+// add(a,b,hello);
+// add(hello,sayHI);
 
-
+// function sayHI(){
+//     console.log("This is callback funtion");
+// }
+// function hello(){
+//     console.log("Hello,Harshit");
+// }
 
 // create a function that display(callback) that print "Welcome to ABES,then call callback which print learning "FSD Workshop"
-
-function
+// function welcome(callback){
+//     console.log("Welcome to ABES");
+//     callback();
+// }
+ 
+// function callback(){
+//     console.log("FSD");
+// }
