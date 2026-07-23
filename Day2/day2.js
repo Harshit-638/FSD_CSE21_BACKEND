55f23942ac52324b5c08a72cd292d4462e158237
Harshit-638 <harshitsharma3264@gmail.com>
2026-07-23 11:03:10 +0530

    Day 2 JS

---
+++ Day2/day2.js
@@ -0,0 +1,41 @@
+//synchronous programming : code is executed line  by line
+// function hello(){
+//     console.log("hello,world");
+
+// }
+// hello();
+// console.log("This is synchronous programming");
+
+//Asynchornous programming : the code is executed line by line, but some 
+//setTimeout(): function that is used to execute the function 
+
+// const hello =()=> {
+//     setTimeout(() => {
+//         console.log("Hello,world");
+//     }, 2000);
+// }
+// hello();
+// console.log("This is asynchronous programming");
+
+// callback, promises, async/await
+
+function add(n1,n2,callback){
+    console.log (n1+n2);   
+    callback();
+}
+
+let a =10;
+let b = 20;
+function sayHI(){
+    console.log("This is callback funtion");
+}
+
+add(a,b,sayHI);
+add(a,b,hello);
+add(hello,sayHI);
+
+
+
+// create a function that display(callback) that print "Welcome to ABES,then call callback which print learning "FSD Workshop"
+
+function
